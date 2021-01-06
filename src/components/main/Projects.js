c97cd9dcdd01885600485987b54d4b9227d00f35
evelandy <evelandy@gmail.com>
2021-01-06 15:11:25 -0600

    project cards fix

--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -19,129 +19,137 @@ export default function Projects() {
                     {matches.small &&
                         <div id="projects" className="projectsContainer">
                             <h2 className='projectsHeader'>Projects</h2>
-                            <ProjectCard 
-                                body={'projectCardBody'}
-                                image={'projectImage'}
-                                title={'projectTitle'}
-                                imgSrc={grdev}
-                                alt={'grdev.com site'}
-                                name={'www.grdev.com'}
-                                site={'https://main.d1uidfzso1gl6d.amplifyapp.com/'}
-                            />
-                            <ProjectCard
-                                body={'projectCardBody'}
-                                image={'projectImage'}
-                                title={'projectTitle'}
-                                imgSrc={conference}
-                                alt={'conference site'}
-                                name={'conference site'}
-                                site={'https://evelandy.github.io/conference-site/'}
-                            />
-                            <ProjectCard
-                                body={'projectCardBody'}
-                                image={'projectImage'}
-                                title={'projectTitle'}
-                                imgSrc={cityGuide}
-                                alt={'city-guide site'}
-                                name={'city-guide site'}
-                                site={'https://evelandy.github.io/City-guide/'}
-                            />
+                            <span className="projectCardContainer">
+                                <ProjectCard 
+                                    body={'projectCardBody'}
+                                    image={'projectImage'}
+                                    title={'projectTitle'}
+                                    imgSrc={grdev}
+                                    alt={'grdev.com site'}
+                                    name={'www.grdev.com'}
+                                    site={'https://main.d1uidfzso1gl6d.amplifyapp.com/'}
+                                />
+                                <ProjectCard
+                                    body={'projectCardBody'}
+                                    image={'projectImage'}
+                                    title={'projectTitle'}
+                                    imgSrc={conference}
+                                    alt={'conference site'}
+                                    name={'conference site'}
+                                    site={'https://evelandy.github.io/conference-site/'}
+                                />
+                                <ProjectCard
+                                    body={'projectCardBody'}
+                                    image={'projectImage'}
+                                    title={'projectTitle'}
+                                    imgSrc={cityGuide}
+                                    alt={'city-guide site'}
+                                    name={'city-guide site'}
+                                    site={'https://evelandy.github.io/City-guide/'}
+                                />
+                            </span>
                         </div>
                     }
                     {matches.medium &&
                         <div id="projects" className="medProjectsContainer">
                             <h2 className='medProjectsHeader'>Projects</h2>
-                            <ProjectCard 
-                                body={'medProjectCardBody'}
-                                image={'medProjectImage'}
-                                title={'medProjectTitle'}
-                                imgSrc={grdev}
-                                alt={'grdev.com site'}
-                                name={'www.grdev.com'}
-                                site={'https://main.d1uidfzso1gl6d.amplifyapp.com/'}
-                            />
-                            <ProjectCard
-                                body={'medProjectCardBody'}
-                                image={'medProjectImage'}
-                                title={'medProjectTitle'}
-                                imgSrc={conference}
-                                alt={'conference site'}
-                                name={'conference site'}
-                                site={'https://evelandy.github.io/conference-site/'}
-                            />
-                            <ProjectCard
-                                body={'medProjectCardBody'}
-                                image={'medProjectImage'}
-                                title={'medProjectTitle'}
-                                imgSrc={cityGuide}
-                                alt={'city-guide site'}
-                                name={'city-guide site'}
-                                site={'https://evelandy.github.io/City-guide/'}
-                            />
+                            <span className="medProjectCardContainer">
+                                <ProjectCard 
+                                    body={'medProjectCardBody'}
+                                    image={'medProjectImage'}
+                                    title={'medProjectTitle'}
+                                    imgSrc={grdev}
+                                    alt={'grdev.com site'}
+                                    name={'www.grdev.com'}
+                                    site={'https://main.d1uidfzso1gl6d.amplifyapp.com/'}
+                                />
+                                <ProjectCard
+                                    body={'medProjectCardBody'}
+                                    image={'medProjectImage'}
+                                    title={'medProjectTitle'}
+                                    imgSrc={conference}
+                                    alt={'conference site'}
+                                    name={'conference site'}
+                                    site={'https://evelandy.github.io/conference-site/'}
+                                />
+                                <ProjectCard
+                                    body={'medProjectCardBody'}
+                                    image={'medProjectImage'}
+                                    title={'medProjectTitle'}
+                                    imgSrc={cityGuide}
+                                    alt={'city-guide site'}
+                                    name={'city-guide site'}
+                                    site={'https://evelandy.github.io/City-guide/'}
+                                />
+                            </span>
                         </div>
                     }
                     {matches.large &&
                         <div id="projects" className="lgProjectsContainer">
                             <h2 className='lgProjectsHeader'>Projects</h2>
-                            <ProjectCard 
-                                body={'lgProjectCardBody'}
-                                image={'lgProjectImage'}
-                                title={'lgProjectTitle'}
-                                imgSrc={grdev}
-                                alt={'grdev.com site'}
-                                name={'www.grdev.com'}
-                                site={'https://main.d1uidfzso1gl6d.amplifyapp.com/'}
-                            />
-                            <ProjectCard
-                                body={'lgProjectCardBody'}
-                                image={'lgProjectImage'}
-                                title={'lgProjectTitle'}
-                                imgSrc={conference}
-                                alt={'conference site'}
-                                name={'conference site'}
-                                site={'https://evelandy.github.io/conference-site/'}
-                            />
-                            <ProjectCard
-                                body={'lgProjectCardBody'}
-                                image={'lgProjectImage'}
-                                title={'lgProjectTitle'}
-                                imgSrc={cityGuide}
-                                alt={'city-guide site'}
-                                name={'city-guide site'}
-                                site={'https://evelandy.github.io/City-guide/'}
-                            />
+                            <span className="lgProjectCardContainer">
+                                <ProjectCard 
+                                    body={'lgProjectCardBody'}
+                                    image={'lgProjectImage'}
+                                    title={'lgProjectTitle'}
+                                    imgSrc={grdev}
+                                    alt={'grdev.com site'}
+                                    name={'www.grdev.com'}
+                                    site={'https://main.d1uidfzso1gl6d.amplifyapp.com/'}
+                                />
+                                <ProjectCard
+                                    body={'lgProjectCardBody'}
+                                    image={'lgProjectImage'}
+                                    title={'lgProjectTitle'}
+                                    imgSrc={conference}
+                                    alt={'conference site'}
+                                    name={'conference site'}
+                                    site={'https://evelandy.github.io/conference-site/'}
+                                />
+                                <ProjectCard
+                                    body={'lgProjectCardBody'}
+                                    image={'lgProjectImage'}
+                                    title={'lgProjectTitle'}
+                                    imgSrc={cityGuide}
+                                    alt={'city-guide site'}
+                                    name={'city-guide site'}
+                                    site={'https://evelandy.github.io/City-guide/'}
+                                />
+                            </span>
                         </div>
                     }
                     {matches.xxl &&
                         <div id="projects" className="XXLProjectsContainer">
                             <h2 className='XXLProjectsHeader'>Projects</h2>
-                            <ProjectCard 
-                                body={'XXLProjectCardBody'}
-                                image={'XXLProjectImage'}
-                                title={'XXLProjectTitle'}
-                                imgSrc={grdev}
-                                alt={'grdev.com site'}
-                                name={'www.grdev.com'}
-                                site={'https://main.d1uidfzso1gl6d.amplifyapp.com/'}
-                            />
-                            <ProjectCard
-                                body={'XXLProjectCardBody'}
-                                image={'XXLProjectImage'}
-                                title={'XXLProjectTitle'}
-                                imgSrc={conference}
-                                alt={'conference site'}
-                                name={'conference site'}
-                                site={'https://evelandy.github.io/conference-site/'}
-                            />
-                            <ProjectCard
-                                body={'XXLProjectCardBody'}
-                                image={'XXLProjectImage'}
-                                title={'XXLProjectTitle'}
-                                imgSrc={cityGuide}
-                                alt={'city-guide site'}
-                                name={'city-guide site'}
-                                site={'https://evelandy.github.io/City-guide/'}
-                            />
+                            <span className="XXLProjectCardContainer">
+                                <ProjectCard 
+                                    body={'XXLProjectCardBody'}
+                                    image={'XXLProjectImage'}
+                                    title={'XXLProjectTitle'}
+                                    imgSrc={grdev}
+                                    alt={'grdev.com site'}
+                                    name={'www.grdev.com'}
+                                    site={'https://main.d1uidfzso1gl6d.amplifyapp.com/'}
+                                />
+                                <ProjectCard
+                                    body={'XXLProjectCardBody'}
+                                    image={'XXLProjectImage'}
+                                    title={'XXLProjectTitle'}
+                                    imgSrc={conference}
+                                    alt={'conference site'}
+                                    name={'conference site'}
+                                    site={'https://evelandy.github.io/conference-site/'}
+                                />
+                                <ProjectCard
+                                    body={'XXLProjectCardBody'}
+                                    image={'XXLProjectImage'}
+                                    title={'XXLProjectTitle'}
+                                    imgSrc={cityGuide}
+                                    alt={'city-guide site'}
+                                    name={'city-guide site'}
+                                    site={'https://evelandy.github.io/City-guide/'}
+                                />
+                            </span>
                         </div>
                     }
                 </Fragment>
